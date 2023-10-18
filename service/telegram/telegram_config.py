04b4903b075fcca9a00d033b3434097ca9dfe05a
Jose Angel <jose.angel.garmon@gmail.com>
2023-10-18 21:54:15 +0200

    Telegram service with commons methods for telegram api and telegram api admin

---
+++ service/telegram/telegram_config.py
@@ -0,0 +1,6 @@
+# Telegram api
+TELEGRAM_API_ENDPOINT: str = "https://api.telegram.org/bot"
+
+# Bot configuration
+BOT_MAX_CONNECTIONS: int = 10
+BOT_DROP_PENDING_UPDATES: bool = False
---
+++ service/telegram/telegram_service_commons.py
@@ -0,0 +1,37 @@
+from service.telegram.telegram_config import TELEGRAM_API_ENDPOINT
+
+
+class TelegramServiceCommons:
+
+    def __init__(self) -> None:
+        pass
+
+    # TODO: Check use of this method with self in child classes
+    @staticmethod
+    def _check_if_telegram_response_is_correct(telegram_response: dict) -> None:
+        """
+        Check if telegram response has ok field and is true
+        :param telegram_response:
+        :return:
+        """
+        if ('ok' not in telegram_response
+                or 'result' not in telegram_response
+                or telegram_response['ok'] is False):
+            raise TelegramServiceCommonsException(
+                f'ok or result are not in telegram response or is false: {telegram_response}'
+            )
+
+    @staticmethod
+    def _build_telegram_api_url_for_method(telegram_bot_token: str, method: str) -> str:
+        """
+        Build the telegram api endpoint for a given method
+        :param telegram_bot_token: bot token for telegram api
+        :param method: telegram api method
+        :return:
+        """
+        return f"{TELEGRAM_API_ENDPOINT}{telegram_bot_token}/{method}"
+
+
+class TelegramServiceCommonsException(Exception):
+    def __init__(self, msg: str):
+        super().__init__(msg)
