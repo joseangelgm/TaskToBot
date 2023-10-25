690885897045046e50457f3c56e43e81e2c74a28
Jose Angel <jose.angel.garmon@gmail.com>
2023-10-25 22:03:34 +0200

    TelegramApiService to manage api requests

---
+++ service/telegram/api/telegram_api_service.py
@@ -0,0 +1,7 @@
+class TelegramApiService:
+    """
+    Class that manage not admin operations with telegram api
+    """
+
+    def __init__(self):
+        self.__token = "" #TODO: recover token from cache
