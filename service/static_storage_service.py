7778d31d2cdf20574bc18701ce9b79fdf5c7b860
Jose Angel <jose.angel.garmon@gmail.com>
2023-10-25 22:02:44 +0200

    Created StaticStorageService to manage data between requests

---
+++ service/static_storage_service.py
@@ -0,0 +1,28 @@
+import logging
+from typing import Any
+
+
+class StaticStorageService:
+
+    __storage: dict = None
+    __LOGGER: logging.Logger = logging.getLogger()
+
+    # TODO: Raise exception
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def initialize(cls) -> None:
+        cls.__LOGGER.log(
+            level=logging.INFO,
+            msg=f"Initializing StaticStorageService..."
+        )
+        cls.__storage: dict = dict()
+
+    @classmethod
+    def get_value(cls, data_key: str) -> Any:
+        return cls.__storage.get(data_key, None)
+
+    @classmethod
+    def set_value(cls, data_key: str, data_value: Any) -> Any:
+        cls.__storage[data_key] = data_value
